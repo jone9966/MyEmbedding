71dc05c7948ca74b8f5d7e513ab060f35eca4f26
jone9966 <jone9966@naver.com>
2022-07-19 15:11:11 +0900

    doc2vec

--- model/Classification.py
+++ model/Classification.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+import torch
+import torch.nn as nn
 
 
 class Classification:
-    def __init__(self):
+    def __init__(self, model, tokenizer, drop_out, num_class):
+        self.model = model
+        self.drop_out = nn.Dropout(drop_out)
+    
+    
+        
+    def forward(self, input_ids, attention_mask):
+        pooler = gpt(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        output = pooler['hidden_states'][-1]
+        batch_size = output.shape[0]
+        output = self.classifier(output.reshape(batch_size, -1))
+        return output
